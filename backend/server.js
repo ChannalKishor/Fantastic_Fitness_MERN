fe24b49870b5751389a5534361bacf99560ee644
kishor channal <kishorchannal@kishors-MacBook-Air.local>
2023-04-19 03:21:08 -0400

    Added Backend User Authentication

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -16,16 +17,19 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.originalUrl)
   next()
 })
 
-// app.get('/', (req, res) => {
-//   res.send('API is running!! ')
-// })
+app.get('/', (req, res) => {
+  res.send('API is running!! ')
+})
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
